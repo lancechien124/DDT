c5bf515621206cbd542adb07459cce0f760d0c30
lancechien124 <35227000+lancechien124@users.noreply.github.com>
2022-10-27 23:57:24 +0800

    Revise output

--- python/Q2.py
+++ python/Q2.py
@@ -5,13 +5,15 @@ msg = "Hello welcome to Cathay 60th year anniversary"
 
 def count_char(str):
     cal = {}
-    full_str = str.replace(" ","")
-    new_str = list(full_str.upper())
-    sorted_list = sorted(new_str)
+    full_str = str.replace(" ","")  ## Remove space
+    new_str = list(full_str.upper()) ## Change to upper case
+    sorted_list = sorted(new_str) ## Sorting char 
     for i in sorted_list:
-        cal[i] = sorted_list.count(i)
-        print ( i +" ", end = "")
-        print (cal[i])
+        cal[i] = sorted_list.count(i) ## Caculate count & save into dict
+
+    for ch in cal:
+        print ( ch +" ", end = "")
+        print (cal[ch])
 
 if __name__ == '__main__':
     new_char = count_char(msg)
